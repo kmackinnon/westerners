bc4aee855f964ef1e3ea849213b24f0731db1b06
JoelChev <joelcheverie@hotmail.com>
2013-11-28 14:27:41 -0500

    Update game.py

--- ass5/game.py
+++ ass5/game.py
@@ -1,16 +1,36 @@
-#! usr/bin/python
+#!/usr/bin/python
+import cgi
+import cgitb; cgitb.enable()   # enable debugging mode
 
-import sys
+form = cgi.FieldStorage() 
+
+coins=int(form.getvalue('coins'))
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello Word - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print coins
+
 
 #get the user's answer to the riddle and convert to lowercase
-answer = raw_input("Enter answer to riddle: ").lower() 
+temp = form.getvalue("answer")
+answer=temp.lower()
+print answer
+
 
 # loop through answer and find substring "map"
 for i in range(len(answer)-2):
-	if answer[i:i+3] == "map":
-		print "Success!"
-		sys.exit(0)
-print "Try Again"
-
-
+        if answer[i:i+3] == "map":
+                coins+=10
+		print '<p>You did it! You have',coins,' coins!</p>'
+	else:
+		coins-=10
+		print '<p> You gave the wrong answer. You have',coins,' coins!</p>'
+		break
+	
+print '</body>'
+print '</html>'
 
