ca36ae704e3056485b728e2636468ce55ee74659
Michael CURTIS <mcurti4@lab1-7.CS.McGill.CA>
2013-11-30 15:08:20 -0500

    changed points to coins

--- ass5/Transfer.py
+++ ass5/Transfer.py
@@ -42,20 +42,13 @@ def printPage(coins,inv1,inv2,inv3,inv4,inv5):
         print "</div>"
 
         print """<form name="game" action="game.py" method="post">"""
-        print "<!-- Hidden tags for inventory -->"
-        print """<input type="hidden" name="Inventory1" value="">"""
-        print """<input type="hidden" name="Inventory2" value="">"""
-        print """<input type="hidden" name="Inventory3" value="">"""
-        print """<input type="hidden" name="Inventory4" value="">"""
-        print """<input type="hidden" name="Inventory5" value="">"""
-
         print "<!-- CGI that which implements game activity and submit button -->"
         print "I have roads but no cars.</br>"
         print "I have lakes but no fish.</br>"
         print "What am I? </br></br>"
         print """Answer: <input type="text" name="answer" method="post" maxlength="10">"""
         #this is what will use the updated coins value to update the page
-        print """<input type="hidden" name="points" value=" """,coins,""" "> """
+        print """<input type="hidden" name="coins" value=" """,coins,""" "> """
 	#added to generate inventorys passed from other pages
         print """<input type="hidden" name="Inventory1" value=" """,inv1,""" "> """
         print """<input type="hidden" name="Inventory2" value=" """,inv2,""" "> """
@@ -67,7 +60,7 @@ def printPage(coins,inv1,inv2,inv3,inv4,inv5):
 
         print """<form name="goNorth" action="http://www.cs.mcgill.ca/~eraso/Grid.html" method="post">"""
       	#this is what will use the updated coins value to update the page
-        print """<input type="hidden" name="points" value=" """,coins,""" "> """
+        print """<input type="hidden" name="coins" value=" """,coins,""" "> """
 	#added to generate inventorys passed from other pages
         print """<input type="hidden" name="Inventory1" value=" """,inv1,""" "> """
         print """<input type="hidden" name="Inventory2" value=" """,inv2,""" "> """
@@ -82,7 +75,7 @@ def printPage(coins,inv1,inv2,inv3,inv4,inv5):
 
         print """<form name="goWest" action="http://cs.mcgill.ca/~cmacdo40/room.html" method="post">"""
       	#this is what will use the updated coins value to update the page
-        print """<input type="hidden" name="points" value=" """,coins,""" "> """
+        print """<input type="hidden" name="coins" value=" """,coins,""" "> """
 	#added to generate inventorys passed from other pages
         print """<input type="hidden" name="Inventory1" value=" """,inv1,""" "> """
         print """<input type="hidden" name="Inventory2" value=" """,inv2,""" "> """
@@ -97,7 +90,7 @@ def printPage(coins,inv1,inv2,inv3,inv4,inv5):
 
         print """<form name="goEast" action="http://www.cs.mcgill.ca/~aturne15/room.html" method="post">"""
         #this is what will use the updated coins value to update the page
-        print """<input type="hidden" name="points" value=" """,coins,""" "> """
+        print """<input type="hidden" name="coins" value=" """,coins,""" "> """
 	#added to generate inventorys passed from other pages
         print """<input type="hidden" name="Inventory1" value=" """,inv1,""" "> """
         print """<input type="hidden" name="Inventory2" value=" """,inv2,""" "> """
@@ -112,7 +105,7 @@ def printPage(coins,inv1,inv2,inv3,inv4,inv5):
 
         print"""<form name="goSouth" action="http://cgi.cs.mcgill.ca/~phende/comp206/room.html" method="post">"""
       	#this is what will use the updated coins value to update the page
-        print """<input type="hidden" name="points" value=" """,coins,""" "> """
+        print """<input type="hidden" name="coins" value=" """,coins,""" "> """
 	#added to generate inventorys passed from other pages
         print """<input type="hidden" name="Inventory1" value=" """,inv1,""" "> """
         print """<input type="hidden" name="Inventory2" value=" """,inv2,""" "> """
