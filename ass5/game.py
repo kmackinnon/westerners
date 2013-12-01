8637ec88c38b3ae781d61370a8be6071d71df652
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-12-01 15:47:15 -0500

    Look, inventory, drop finally working. Just need to do pickup in game.py

--- ass5/game.py
+++ ass5/game.py
@@ -5,7 +5,8 @@ import csv
 
 form = cgi.FieldStorage()
 
-coins=int(form.getvalue('coins'))
+if form.getvalue('coins') is not None:
+	coins=int(form.getvalue('coins'))
 
 Inventory1=form.getvalue("Inventory1")
 Inventory2=form.getvalue("Inventory2")
@@ -23,10 +24,14 @@ print """<link rel="stylesheet" type="text/css" href="roomstyle.css">"""
 print '</head>'
 print '<body>'
 
-#TO DO:
-	#Change the parameter here to a List, no a "Listing" as it is here. That will resolve logic issues hopefully
-def printPage(coins, Inventory1, Inventory2, Inventory3, Inventory4, Inventory5):
-        #this will regenerate the page
+for i in range(len(InventoryList)):
+	if InventoryList[i] == "None":
+		InventoryList[i]=""
+
+
+#TODO: param to list
+# regenerates the page
+def printPage(coins, InventoryList):
         print """<div id = "header">"""
         print '<h1> The Room </h1>'
         print '</div>'
@@ -40,18 +45,17 @@ def printPage(coins, Inventory1, Inventory2, Inventory3, Inventory4, Inventory5)
 
         print """<form name="game" action="game.py" method="post">"""
         print "<!-- Hidden tags for inventory -->"
-        print """<input type="hidden" name="Inventory1" value=" """,Inventory1,""" ">"""
-        print """<input type="hidden" name="Inventory2" value=" """,Inventory2,""" ">"""
-        print """<input type="hidden" name="Inventory3" value=" """,Inventory3,""" ">"""
-        print """<input type="hidden" name="Inventory4" value=" """,Inventory4,""" ">"""
-        print """<input type="hidden" name="Inventory5" value=" """,Inventory5,""" ">"""
-
+        print """<input type="hidden" name="Inventory1" value=""",InventoryList[0],""">"""
+        print """<input type="hidden" name="Inventory2" value=""",InventoryList[1],""">"""
+        print """<input type="hidden" name="Inventory3" value=""",InventoryList[2],""">"""
+        print """<input type="hidden" name="Inventory4" value=""",InventoryList[3],""">"""
+        print """<input type="hidden" name="Inventory5" value=""",InventoryList[4],""">"""
         print "<!-- CGI which implements game activity and submit button -->"
         print "I have roads but no cars.</br>"
         print "I have lakes but no fish.</br>"
         print "What am I? </br></br>"
         print """Answer: <input type="text" name="answer" method="post" maxlength="5">"""
-        #this is what will use the updated coins value to update the page
+        #updates coins value to update the page
         print """<input type="hidden" name="coins" value=" """,coins,""" "> """
         print """<input type="hidden" name="select" value="riddle">"""
         print """<input type="submit" value="Submit">"""
@@ -59,15 +63,14 @@ def printPage(coins, Inventory1, Inventory2, Inventory3, Inventory4, Inventory5)
 
         print """<form name="command" action="game.py" method="post">"""
         print """<!--Hidden tags for inventory -->"""
-        print """<input type="hidden" name="Inventory1" value=" """,Inventory1,""" ">"""
-        print """<input type="hidden" name="Inventory2" value=" """,Inventory2,""" ">"""
-        print """<input type="hidden" name="Inventory3" value=" """,Inventory3,""" ">"""
-        print """<input type="hidden" name="Inventory4" value=" """,Inventory4,""" ">"""
-        print """<input type="hidden" name="Inventory5" value=" """,Inventory5,""" ">"""
-
-        print """<!-- CGI that implements the game commands -->"""
+        print """<input type="hidden" name="Inventory1" value=""",InventoryList[0],""">"""
+        print """<input type="hidden" name="Inventory2" value=""",InventoryList[1],""">"""
+        print """<input type="hidden" name="Inventory3" value=""",InventoryList[2],""">"""
+        print """<input type="hidden" name="Inventory4" value=""",InventoryList[3],""">"""
+        print """<input type="hidden" name="Inventory5" value=""",InventoryList[4],""">"""
+        print """<!-- CGI which implements the command -->"""
         print """Command: <input type="text" name="command" method="post" maxlength="10">"""
-        #I pass in the coins again too.
+        #pass in the coins again too.
         print """<input type="hidden" name="coins" value =" """,coins,""" "> """
         print """<input type="hidden" name="select" value="command">"""
         print """<input type="submit" value="Command">"""
@@ -76,79 +79,78 @@ def printPage(coins, Inventory1, Inventory2, Inventory3, Inventory4, Inventory5)
         print"</br>"
 
 
-#Now I have to handle the two forms
-
-#this handles if a user enters an answer
+#Now have to handle the two forms
 select=form.getvalue("select")
 
+# if the user tries to answer the riddle
 if select=="riddle":
-	
-        #get the user's answer to the riddle and convert to lowercase
-        answer=form.getvalue("answer").lower()
-        #If a user has coins, they can interact.
-        if coins!=0:
-                # loop through answer and find substring "map"
-                if "map" in answer:
-                        coins+=10
-                        printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
-                        print '<font color="green"><p>That is correct! You have earned 10 more coins. You have',coins,' coins!</p></font>'
-                
-		else:
-                        coins-=10
-                        printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
-                        print '<font color="red"><p> You gave the wrong answer. You have lost 10 coins. You have',coins,' coins!</p></font>'
-  
-	#if a user has no coins, they cannot interact.
+	#get the user's answer to the riddle and convert to lowercase
+    answer=form.getvalue("answer").lower()
+    #If a user has coins, they can interact.
+    if coins != 0:
+        # loop through answer and find substring "map"
+        if "map" in answer:
+            coins+=10
+            printPage(coins,InventoryList)
+            print '<font color="green"><p>That is correct! You have earned 10 more coins. You have',coins,' coins!</p></font>'
         else:
-                printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
+            coins-=10
+            printPage(coins,InventoryList)
+            print '<font color="red"><p> You gave the wrong answer. You have lost 10 coins. You have',coins,' coins!</p></font>'
+	#if a user has no coins, they cannot interact.
+    else:
+        printPage(coins,InventoryList)
 
-#this handles if a user enters a command.        
+# if the user enters a command       
 elif select=="command":
-
-        command = form.getvalue("command").lower()
-
-        if command == "look":
-                printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
+	command = form.getvalue("command").lower()
+	if command == "look":
+		printPage(coins,InventoryList)
 		file='inventory.csv'
 		f=open(file, 'r')
 		count=1
-    		for line in f:
+		for line in f:
 			print count,"."
-        		print line
+			print line
 			print "</br>"
 			count+=1
-
-	#get all the inventory data and save to each inventory number 
+	elif command[:6] == "pickup":
+		num = int(command[7:])
+		print "Test pickup"
+		# remove n items from inventory and add it to first hidden inventory slot
+		# if there are no slots available, error message and item should not be removed from csv.
+		# room redisplayed
 	
-        elif command[:6] == "pickup":
-                num = int(command[7:])
-                # remove n items from inventory and add it to first hidden inventory slot
-                # if there are no slots available, error message and item should not be removed from csv.
-                # room redisplayed
-
-        elif command[:4] == "drop":
+	elif command[:4] == "drop":
 		num = int(command[5:])
-		print InventoryList[num-1]
+		
+		print InventoryList
+		if isinstance(InventoryList[num-1], str):
+			print "Vybihal"
+			# We know that InventoryList[num-1] is a string which is equal to "None"
+			print [ord(c) for c in InventoryList[num-1]]
+			if InventoryList[num-1] == "None":
+				print "FML"
+
 		if 1<=num<=5:
-                	if InventoryList[num-1] is not None:
+			if InventoryList[num-1]: #enters if statement when not None
 				file='inventory.csv'
-				beginning=InventoryList[num-1]
-				end="\n"
-				insert=beginning+end
-				f=open(file,'ab').write(insert)
+				f=open(file,'ab')
+				f.write(str(InventoryList[num-1]))
+				f.write("\n")
 				InventoryList[num-1]=""
-				printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
+				printPage(coins,InventoryList)
+				print InventoryList[num-1]
 				print "Item was dropped!"
 			else:
-				print "<p> No item to drop!</p>"
-				
+				printPage(coins,InventoryList)
+				print "No item to drop!"
 		else:
 			print "Please enter a valid pickup number from 1-5."
-                
-
-        elif command == "inventory":
-		printPage(coins,Inventory1,Inventory2,Inventory3,Inventory4,Inventory5)
-                print "1.", Inventory1
+	
+	elif command == "inventory":
+		printPage(coins,InventoryList)
+		print "1.", Inventory1
 		print "</br>"
 		print "2.", Inventory2
 		print "</br>"
@@ -157,11 +159,7 @@ elif select=="command":
 		print "4.", Inventory4
 		print "</br>"
 		print "5.", Inventory5
-                # displays the items the user has to the screen
-                # room redisplayed
-
-
+        # displays the items the user has to the screen
+        # room redisplayed
 print '</body>'
 print '</html>'
-
-	
